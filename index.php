d8338892a61920f078c89b8aa901c81cdae20529
Azretshakirov <92852391+Azretshakirov@users.noreply.github.com>
2021-11-16 19:21:09 +0600

    Update index.php

--- index.php
+++ index.php
@@ -1,28 +1,28 @@
+<!DOCTYPE html>
 <html>
 <head>
 <title>Фоновое изображение</title>
 
  </head>
- <body bgcolor=white background="https://funart.pro/uploads/posts/2021-04/1618496600_58-funart_pro-p-oboi-fon-neonovii-fon-62.jpg">
+ <body bgcolor=white background="https://cdn.pixabay.com/photo/2016/06/02/02/33/triangles-1430105__480.png">
 
-<h1>PORTFOLIO</h1>
+<center><h1>PORTFOLIO</h1></center>
 
 <hr>
 
 <h2>AZRET</h2>
 
-<p><img src="https://thumb.tildacdn.com/tild3535-3966-4038-a163-636265636439/-/resize/560x/-/format/webp/IMG_1581.JPG"></p>
- <style>
+<p><img src="https://thumb.tildacdn.com/tild3535-3966-4038-a163-636265636439/-/resize/560x/-/format/webp/IMG_1581.JPG" alt="portfolio image" width="220" height="200"></p>
 
 <hr>
 
-<p>Name.  :   Azret      </p>
+<p>Name  :   Azret      </p>
 <hr>
 <p>Surname:   Shakirov     </p>
 <hr>
-<p>Address    :   Bugu-Ene 64             </p>
+<p>Address    :   Bugu-Ene 63            </p>
 <hr>
-<p>Class  :.  Csc-20a.       </p>
+<p>Class  :  Csc-20a.       </p>
 <hr>
 
 </body>
