1edb5786378805535a1a9fde45491b78e93b587c
Azretshakirov <92852391+Azretshakirov@users.noreply.github.com>
2021-11-16 19:48:20 +0600

    Update index.php

--- index.php
+++ index.php
@@ -1,10 +1,9 @@
-<!DOCTYPE html>
 <html>
 <head>
 <title>Фоновое изображение</title>
 
  </head>
- <body bgcolor=white background="https://cdn.pixabay.com/photo/2016/06/02/02/33/triangles-1430105__480.png">
+ <body bgcolor=white background="https://kartinkin.net/uploads/posts/2021-07/1626254156_20-kartinkin-com-p-korolevskii-fon-krasivo-30.jpg">
 
 <center><h1>PORTFOLIO</h1></center>
 
@@ -12,7 +11,7 @@
 
 <h2>AZRET</h2>
 
-<p><img src="https://thumb.tildacdn.com/tild3535-3966-4038-a163-636265636439/-/resize/560x/-/format/webp/IMG_1581.JPG" alt="portfolio image" width="220" height="200"></p>
+<p><img src="https://thumb.tildacdn.com/tild3535-3966-4038-a163-636265636439/-/resize/560x/-/format/webp/IMG_1581.JPG" alt="portfolio image" width="220" height="200" style= float:right></p>
 
 <hr>
 
