34705785895f89bca2423acd64f3e5b16d01126f
Azretshakirov <92852391+Azretshakirov@users.noreply.github.com>
2021-11-16 18:55:03 +0600

    Update index.php

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 <title>Фоновое изображение</title>
 
  </head>
- <body bgcolor="white background="https://funart.pro/uploads/posts/2021-04/1618496600_58-funart_pro-p-oboi-fon-neonovii-fon-62.jpg">
+ <body bgcolor=white background="https://funart.pro/uploads/posts/2021-04/1618496600_58-funart_pro-p-oboi-fon-neonovii-fon-62.jpg">
 
 <h1>PORTFOLIO</h1>
 
@@ -12,6 +12,13 @@
 <h2>AZRET</h2>
 
 <p><img src="https://thumb.tildacdn.com/tild3535-3966-4038-a163-636265636439/-/resize/560x/-/format/webp/IMG_1581.JPG"></p>
+ <style>
+img {
+  border-radius: 50%;
+  display: block;
+  margin-left: auto;
+}
+</style> 
 
 <hr>
 
