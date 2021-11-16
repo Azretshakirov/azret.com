17b879f1554747163b885c4155536a348136d4d5
Azretshakirov <92852391+Azretshakirov@users.noreply.github.com>
2021-11-16 18:42:46 +0600

    Create index.php

---
+++ index.php
@@ -0,0 +1,29 @@
+!DOCTYPE html>
+<html>
+<head>
+<title>Фоновое изображение</title>
+
+ </head>
+ <body bgcolor=white background="https://funart.pro/uploads/posts/2021-04/1618496600_58-funart_pro-p-oboi-fon-neonovii-fon-62.jpg">
+
+<h1>PORTFOLIO</h1>
+
+<hr>
+
+<h2>Personal data</h2>
+
+<p><img src="https://www.meme-arsenal.com/memes/a13202136f3ed3b2ea0135fc70d4d239.jpg" alt="portfolio image" width="220" height="200"></p>
+
+<hr>
+
+<p>Name.  :   Azret      </p>
+<hr>
+<p>Surname:   Shakirov     </p>
+<hr>
+<p>Age    :   17             </p>
+<hr>
+<p>Class  :.  Csc-20a.       </p>
+<hr>
+
+</body>
+</html>
