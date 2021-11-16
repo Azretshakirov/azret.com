d148b56c8539630eeb0e218d9a6381d9adac4b7b
Azretshakirov <92852391+Azretshakirov@users.noreply.github.com>
2021-11-16 18:52:27 +0600

    Update index.php

--- index.php
+++ index.php
@@ -3,13 +3,13 @@
 <title>Фоновое изображение</title>
 
  </head>
- <body bgcolor=white background="https://funart.pro/uploads/posts/2021-04/1618496600_58-funart_pro-p-oboi-fon-neonovii-fon-62.jpg">
+ <body bgcolor="white background="https://funart.pro/uploads/posts/2021-04/1618496600_58-funart_pro-p-oboi-fon-neonovii-fon-62.jpg">
 
 <h1>PORTFOLIO</h1>
 
 <hr>
 
-<h2>Personal data</h2>
+<h2>AZRET</h2>
 
 <p><img src="https://thumb.tildacdn.com/tild3535-3966-4038-a163-636265636439/-/resize/560x/-/format/webp/IMG_1581.JPG"></p>
 
@@ -19,7 +19,7 @@
 <hr>
 <p>Surname:   Shakirov     </p>
 <hr>
-<p>Age    :   17             </p>
+<p>Address    :   Bugu-Ene 63             </p>
 <hr>
 <p>Class  :.  Csc-20a.       </p>
 <hr>
